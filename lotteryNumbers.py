36fd037f0ea13aeed09178a050b2ccd140c0a2bc
v.karakai <v.karakai@pin-up.tech>
2024-06-29 12:37:39 +0300

    [4.2] implemented params validation

--- lotteryNumbers.py
+++ lotteryNumbers.py
@@ -2,4 +2,13 @@ import random
 
 
 def get_number_tickets(min_number, max_number, quantity):
-    return random.sample(range(min_number, max_number), quantity)
+    if not params_validate(max_number, min_number, quantity):
+        return []
+    numbers = random.sample(range(min_number, max_number + 1), quantity)
+
+    return sorted(numbers)
+
+
+def params_validate(max_number, min_number, quantity):
+    return min_number < max_number or min_number > 1 or max_number < 1000 or quantity > 1
+
